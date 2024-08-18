5a8efdb1d2e1770547e40109d88dbe375da15da7
mateusz779 <mateusz.kedziora@mkedziora.pl>
2024-08-18 11:04:32 +0000

    update

---
+++ _next/static/0U8OW02GoAlLBtkVYf4nv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d74d93f78a75c337.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

06bc05028d15627fee5d770ea353b7eb9fb99220
mateusz779 <mateusz.kedziora@mkedziora.pl>
2024-08-18 09:24:54 +0000

    update

---
+++ _next/static/chunks/app/page-920d7cfabf55910d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1931,8132,1392,357,8593,4654,4834,1031,1658],{7874:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,2971,7023,1744],function(){return n(n.s=7874)}),_N_E=n.O()}]);

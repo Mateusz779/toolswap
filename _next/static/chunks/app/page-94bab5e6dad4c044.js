6fcfa432d5c3b7f87d2064e46ff840f223ffe130
mateusz779 <mateusz.kedziora@mkedziora.pl>
2024-08-19 19:10:00 +0000

    update

--- _next/static/chunks/app/page-94bab5e6dad4c044.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1931,1392,8132,4602,9212,357,8593,4654,4834,1658,1031],{3407:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,2971,7023,1744],function(){return n(n.s=3407)}),_N_E=n.O()}]);
